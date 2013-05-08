adf3b49eb827a4eefbe5f681fc85b7a572938f84
DamnDam <gnieofmars@gmail.com>
2013-05-08 10:53:04 +0200

    Improve scoring + End game animations

--- web/js/main.js
+++ web/js/main.js
@@ -35,11 +35,14 @@ $(document).ready(function() {
             var url = data[position];
             data.splice(position, 1);
             item.attr('data-id', url.id);
-            var redClass = 'red' + Math.ceil(Math.random() * 3);
+            var range = Math.ceil(Math.random() * 3);
+            var points = range == 1 ? 1 : range == 2 ? 3 : 5;
+            var redClass = 'red' + range;
             switch (url.id) {
                 case -1:
                     item.addClass('real');
                     item.addClass(redClass);
+                    item.attr('data-points', points);
                     a.attr('href', '');
                     break;
                 case 0:
@@ -48,6 +51,7 @@ $(document).ready(function() {
                 default:
                     item.addClass('real');
                     item.addClass(redClass);
+                    item.attr('data-points', points);
                     a.attr('href', url.href);
                     break;
             }
@@ -77,7 +81,7 @@ $(document).ready(function() {
 	
 	var timeSpan = $('#time');
 	
-	var time = 10;
+	var time = 2;
 
     timeSpan.html(time);
 
@@ -87,24 +91,13 @@ $(document).ready(function() {
     var intervalId;
 
     $('span.real').on('mousedown', function() {
-        if (!countdownStarted) {
-            intervalId = setInterval(function() {
-                time -= 1;
-                timeSpan.html(time);
-                if (time == 0) {
-                    clearInterval(intervalId);
-                    // End of the game
-                    endOfTheGame();
-                }
-            }, 1000);
-            countdownStarted = true;
-        }
+        startCounter();
         if ($(this).hasClass('checked')) {
             return;
         }
         $(this).addClass('checked');
         $(this).removeClass('real');
-        score++;
+        score += parseInt($(this).attr('data-points'));
         var a = $(this).find('a');
         console.log($(this).attr('data-id'));
         if ($(this).attr('data-id') > 0) {
@@ -118,19 +111,41 @@ $(document).ready(function() {
         updateScore();
     });
 
+    $('span:not(.real)').on('mousedown', function() {
+        startCounter();
+        score -= 10;
+        updateScore();
+    });
+
     var scoreSpan = $('#score');
 
     function updateScore() {
         scoreSpan.html(score);
     };
 
+    function startCounter () {
+        if (!countdownStarted) {
+            intervalId = setInterval(function() {
+                time -= 1;
+                timeSpan.html(time);
+                if (time == 0) {
+                    clearInterval(intervalId);
+                    // End of the game
+                    endOfTheGame();
+                }
+            }, 1000);
+            countdownStarted = true;
+        }
+    };
+
     function endOfTheGame() {
         // Disparition de la grille
-        $('#grid').fadeOut(3000);
+        $('#grid').fadeOut(5000);
         // Interruption du jeu
         $('span.real').off('mousedown');
+        $('span:not(.real)').off('mousedown');
         // Apparition du message de game over
         $('#final_score').html(score);
-        $('#gameover').fadeIn('fast');
+        $('#gameover').slideDown(2000);
     };
 });
