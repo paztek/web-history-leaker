130f1254164fa0a025fa95960908fbc4022b9675
DamnDam <gnieofmars@gmail.com>
2013-05-08 03:44:30 +0200

    Basic wepapp

--- web/index.php
+++ web/index.php
@@ -5,13 +5,84 @@ $app = require_once __DIR__.'/../bootstrap.php';
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 
+use WHL\Model\Game;
+use WHL\Model\Url;
+
 Request::enableHttpMethodParameterOverride();
 
 /*
  * Routes
  */
 $app->get('/', function (Request $request) use($app) {
-    return $app['twig']->render('game.html.twig');
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->get('/new', function (Request $request) use($app) {
+    return $app['twig']->render('new.html.twig');
+});
+
+$app->post('/create', function (Request $request) use($app) {
+
+    $em = $app['orm.em'];
+
+    $game = new Game();
+    $game->setPlayed(false);
+    $em->persist($game);
+
+    $text = $request->request->get('urls');
+    $urlsData = explode("\r\n", $text);
+
+    foreach ($urlsData as $urlData) {
+        $url = new Url();
+        $url->setChecked(false);
+        $url->setHref($urlData);
+        $url->setGame($game);
+        $em->persist($url);
+    }
+
+    $em->flush();
+
+    return $app->redirect("/report/" . $game->getId());
+});
+
+$app->get('/game/{id}', function (Request $request, $id) use($app) {
+
+    $em = $app['orm.em'];
+
+    $game = $em->getRepository('WHL\Model\Game')->find($id);
+
+    $game->setPlayed(true);
+
+    $em->flush();
+
+    $data = [];
+
+    foreach ($game->getUrls() as $url) {
+        $data[] = array( "id" => $url->getId(), "href" => $url->getHref());
+    }
+
+    return $app['twig']->render('game.html.twig', array('data' => $data));
+});
+
+$app->get('/report/{id}', function (Request $request, $id) use($app) {
+
+    $em = $app['orm.em'];
+
+    $game = $em->getRepository('WHL\Model\Game')->find($id);
+
+    return $app['twig']->render('report.html.twig', array('game' => $game));
+});
+
+$app->put('/url/{id}', function (Request $request, $id) use($app) {
+    $em = $app['orm.em'];
+
+    $url = $em->getRepository('WHL\Model\Url')->find($id);
+
+    $url->setChecked(true);
+
+    $em->flush();
+
+    return new \Symfony\Component\HttpFoundation\Response("", 200);
 });
 
 /*
--- web/js/main.js
+++ web/js/main.js
@@ -1,28 +1,32 @@
 $(document).ready(function() {
 	
-	$('a').on('click', function(event) {
+	$('span.item a').on('click', function(event) {
 		event.preventDefault();
 	});
 	
-	$('a').hover(function() {
+	$('span.item a').hover(function() {
 		$(this).data('href', $(this).attr('href'));
 		$(this).attr('href', '#');
 	}, function() {
 		$(this).attr('href', $(this).data('href'));
 	});
+
+    // Construction de la grille
+    console.log(urls);
+
 	
 	var timeSpan = $('#time');
 	
 	var time= 10;
 
-    var urls = [];
+    var urlsChecked = [];
 	
 	var id = setInterval(function() {
 		time -= 1;
 		timeSpan.html(time);
         if (time == 0) {
             clearInterval(id);
-            console.log(urls);
+            console.log(urlsChecked);
         }
 	}, 1000);
 
@@ -38,7 +42,7 @@ $(document).ready(function() {
         var a = $(this).find('a');
         if (a.length > 0) {
             console.log(a.first().data('href'));
-            urls.push(a.first().data('href'));
+            urlsChecked.push(a.first().data('href'));
         }
         updateScore();
     });
